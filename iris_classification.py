4af2d138b4d34d77c17ad8b0062422a430b5e1e2
VTiganov <vadimyxy@gmail.com>
2024-11-02 20:56:45 +0300

    modified:   iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
 
 iris = load_iris()
 
@@ -12,3 +14,12 @@ iris_df['species'] = iris_df['species'].map({0: 'setosa', 1: 'versicolor', 2: 'v
 # print("Dataset shape:", iris_df.shape)
 # print(iris_df.isnull().sum())
 # CHECKING DATAFRAME
+
+X = iris_df.drop('species', axis=1)
+y = iris_df['species']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# print("Training set shape:", X_train.shape)
+# print("Testing set shape:", X_test.shape)
+# 20% TEST 80% TRAIN
