6751d79239f2cd840626d0ca7cd293278add12a8
VTiganov <vadimyxy@gmail.com>
2024-11-02 20:58:20 +0300

    modified:   iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import accuracy_score, classification_error, confusion_matrix
 
 iris = load_iris()
 
