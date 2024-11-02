6d675f79e1d5f3c2c134a81a37a1a0f7e8c29f7e
VTiganov <vadimyxy@gmail.com>
2024-11-02 21:09:19 +0300

    modified:   iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import accuracy_score, classification_error, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 iris = load_iris()
 
@@ -24,3 +24,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # print("Training set shape:", X_train.shape)
 # print("Testing set shape:", X_test.shape)
 # 20% TEST 80% TRAIN
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+class_report = classification_report(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Confusion Matrix:\n", conf_matrix)
+print("Classification Report:\n", class_report)
+
