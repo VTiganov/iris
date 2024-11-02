38c67227d4707cf65ac7c102c22c628292316154
VTiganov <vadimyxy@gmail.com>
2024-11-02 20:51:42 +0300

    modified:   iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -1,2 +1,11 @@
 import pandas as pd
 from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+iris_df['species'] = iris.target
+
+iris_df['species'] = iris_df['species'].map({0: 'setosa', 1: 'versicolor', 2: 'virginica'})
+print(iris_df.head())
+
