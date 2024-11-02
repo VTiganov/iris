419e94fde04b343fae4b6a14b2bf210df0992831
VTiganov <vadimyxy@gmail.com>
2024-11-02 20:54:26 +0300

    modified:   iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -7,5 +7,8 @@ iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 iris_df['species'] = iris.target
 
 iris_df['species'] = iris_df['species'].map({0: 'setosa', 1: 'versicolor', 2: 'virginica'})
-print(iris_df.head())
 
+# print(iris_df.describe())
+# print("Dataset shape:", iris_df.shape)
+# print(iris_df.isnull().sum())
+# CHECKING DATAFRAME
